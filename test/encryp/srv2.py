2700f5dbaab7983275afe7f3863a552953c3ecb4
ziyagadim <ziya.q012@gmail.com>
2024-05-11 12:41:50 +0400

    Final

--- test/encryp/srv2.py
+++
@@ -1,3 +0,0 @@
-import os
-
-print(os.path.getsize(r"C:\Users\Student\Downloads\isos\Windows 10.iso"))
--- test/psutil/main.py
+++
@@ -1,7 +0,0 @@
-import psutil
-
-running_processes = psutil.process_iter()
-for process in running_processes:
-    print(process)
-
-psutil.Process(17100).kill()
